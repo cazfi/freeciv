8763b71cdc4e226e507df8b2abd1bfc83707c628
Marko Lindqvist <cazfi74@gmail.com>
2022-04-16 13:53:07 +0300

    Improve macapp.sh
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- macOS/homebrew/macapp.sh
+++ macOS/homebrew/macapp.sh
@@ -6,16 +6,25 @@
 # "master" works too, if you want development version
 HBVER="3.4.6"
 
-cd "$(dirname $0)/../../"
+# Parts of this can be tested on linux too.
+if test "$(uname)" = "Linux" ; then
+  APPROOT="$HOME/homebrew"
+else
+  APPROOT="/Applications"
+fi
 
-if ! test -x "./fc_version" ; then
-  echo "No executable ./fcversion at $(pwd)" >&2
+cd "$(dirname $0)"
+
+SRCROOT="$(cd ../.. && pwd)"
+
+if ! test -x "${SRCROOT}/fc_version" ; then
+  echo "No executable ${SRCROOT}/fcversion at $(pwd)" >&2
   exit 1
 fi
 
-FCVER="$(./fc_version)"
+FCVER="$(${SRCROOT}/fc_version)"
 
-MAINDIR="/Applications/freeciv-${FCVER}.app"
+MAINDIR="${APPROOT}/freeciv-${FCVER}.app"
 
 if ! mkdir -p "$MAINDIR/Contents" ; then
   echo "Failed to create \"$MAINDIR/Contents\"" >&2
@@ -52,6 +61,7 @@ PACKAGES="\
  freetype \
  gettext \
  glib \
+ gtk4 \
  gtk+3 \
  icu4c \
  pango \
@@ -75,7 +85,22 @@ fi
 # Standard naming of the tools (gmake -> make etc)
 export PATH="${MAINDIR}/Resources/opt/libtool/libexec/gnubin:$PATH"
 
-if ! ./configure --prefix="${MAINDIR}/Contents" --bindir="${MAINDIR}/MacOS" \
+if ! mkdir -p build ; then
+  echo "Failed to create the build directory" >&2
+  exit 1
+fi
+
+cd build
+
+if ! test -x ${SRCROOT}/configure ; then
+  # Assume this to be a git checkout - generate required bootstrap files
+  if ! ${SRCROOT}/autogen.sh --no-configure-run ; then
+    echo "Autogen.sh failed" >&2
+    exit 1
+  fi
+fi
+
+if ! ${SRCROOT}/configure --prefix="${MAINDIR}/Contents" --bindir="${MAINDIR}/MacOS" \
  PATH="${MAINDIR}/Contents/Resources/bin:${MAINDIR}/Contents/Resources/opt/qt6/bin/:/usr/bin:/bin:/usr/sbin:/sbin:/Library/Apple/usr/bin" \
  LDFLAGS="-L${MAINDIR}/Contents/Resources/opt/qt6/lib -L${MAINDIR}/Contents/Resources/lib" \
  PKG_CONFIG_PATH="${MAINDIR}/Contents/Resources/opt/icu4c/lib/pkgconfig:$PKG_CONFIG_PATH" \
@@ -86,7 +111,7 @@ then
 fi
 
 if ! make -j$(sysctl -n hw.logicalcpu) ; then
-  echo"Freeciv make failed!" >&2
+  echo "Freeciv make failed!" >&2
   exit 1
 fi
 
