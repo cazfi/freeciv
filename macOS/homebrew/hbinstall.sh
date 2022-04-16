644ae6165ebd994b493e776e276d42c3a9894c6e
Marko Lindqvist <cazfi74@gmail.com>
2022-04-16 15:02:38 +0300

    Rework homebrew install
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

---
+++ macOS/homebrew/hbinstall.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+# Install homebrew for building MacOS freeciv .app
+# WIP
+
+# Can be "master", if you want development version
+HBVER="3.4.6"
+
+if test "$1" = "" ; then
+  echo "No installation directory given." >&2
+  exit 1
+fi
+
+MAINDIR="$1"
+
+if ! mkdir -p "${MAINDIR}/Contents" ; then
+  echo "Failed to create \"${MAINDIR}/Contents\"" >&2
+  exit 1
+fi
+
+if ! mkdir -p "${MAINDIR}/Resources" ; then
+  echo "Failed to create \"${MAINDIR}/Resources\"" >&2
+  exit 1
+fi
+
+if ! mkdir -p "${MAINDIR}/MacOS" ; then
+  echo "Failed to create \"${MAINDIR}/MacOS\"" >&2
+  exit 1
+fi
+
+if ! curl -L "https://github.com/Homebrew/brew/tarball/${HBVER}" |
+     tar xz --strip 1 -C "${MAINDIR}/Resources"
+then
+  echo "Homebrew install failed" >&2
+  exit 1
+fi
+
+eval "$("${MAINDIR}/Resources/bin/brew" shellenv)"
+
+PACKAGES="\
+ lua@5.4 \
+ autoconf \
+ automake \
+ gettext \
+ libtool \
+ pkg-config \
+ atk \
+ freetype \
+ gettext \
+ glib \
+ gtk+3 \
+ icu4c \
+ pango \
+ sdl2_gfx \
+ sdl2_image \
+ sdl2_mixer \
+ sdl2_ttf \
+ qt@6 \
+"
+
+if ! brew fetch $PACKAGES ; then
+  echo "Homebrew fetching packages failed." >&2
+  exit 1
+fi
+
+if ! brew install $PACKAGES ; then
+  echo "Homebrew packages installation failed." >&2
+  exit 1
+fi
--- macOS/homebrew/macapp.sh
+++ macOS/homebrew/macapp.sh
@@ -3,9 +3,6 @@
 # Build MacOS freeciv .app
 # WIP
 
-# "master" works too, if you want development version
-HBVER="3.4.6"
-
 # Parts of this can be tested on linux too.
 if test "$(uname)" = "Linux" ; then
   APPROOT="$HOME/homebrew"
@@ -13,74 +10,33 @@ else
   APPROOT="/Applications"
 fi
 
-cd "$(dirname $0)"
+cd "$(dirname "$0")" || exit 1
 
 SRCROOT="$(cd ../.. && pwd)"
 
 if ! test -x "${SRCROOT}/fc_version" ; then
-  echo "No executable ${SRCROOT}/fcversion at $(pwd)" >&2
+  echo "No executable ${SRCROOT}/fcversion" >&2
   exit 1
 fi
 
-FCVER="$(${SRCROOT}/fc_version)"
+FCVER="$("${SRCROOT}/fc_version")"
 
-MAINDIR="${APPROOT}/freeciv-${FCVER}.app"
-
-if ! mkdir -p "$MAINDIR/Contents" ; then
-  echo "Failed to create \"$MAINDIR/Contents\"" >&2
-  exit 1
+if test "$1" != "" ; then
+  MAINDIR="$1"
+else
+  MAINDIR="${APPROOT}/freeciv-${FCVER}.app"
 fi
 
-if ! mkdir -p "$MAINDIR/Resources" ; then
-  echo "Failed to create \"$MAINDIR/Resources\"" >&2
-  exit 1
+if ! test -d "${MAINDIR}" ; then
+  if ! ./hbinstall.sh "${MAINDIR}" ; then
+    echo "Homebrew environment installation to \"${MAINDIR}\" failed" >&2
+    exit 1
+  fi
+else
+  echo "\${MAINDIR}\" exist. Skipping homebrew install"
 fi
 
-if ! mkdir -p "$MAINDIR/MacOS" ; then
-  echo "Failed to create \"$MAINDIR/MacOS\"" >&2
-  exit 1
-fi
-
-if ! curl -L https://github.com/Homebrew/brew/tarball/$HBVER |
-     tar xz --strip 1 -C "$MAINDIR/Resources"
-then
-  echo "Homebrew install failed" >&2
-  exit 1
-fi
-
-eval "$($MAINDIR/Resources/bin/brew shellenv)"
-
-PACKAGES="\
- lua@5.4 \
- autoconf \
- automake \
- gettext \
- libtool \
- pkg-config \
- atk \
- freetype \
- gettext \
- glib \
- gtk4 \
- gtk+3 \
- icu4c \
- pango \
- sdl2_gfx \
- sdl2_image \
- sdl2_mixer \
- sdl2_ttf \
- qt@6 \
-"
-
-if ! brew fetch $PACKAGES ; then
-  echo "Homebrew fetching packages failed." >&2
-  exit 1
-fi
-
-if ! brew install $PACKAGES ; then
-  echo "Homebrew packages installation failed." >&2
-  exit 1
-fi
+eval "$("${MAINDIR}/Resources/bin/brew" shellenv)"
 
 # Standard naming of the tools (gmake -> make etc)
 export PATH="${MAINDIR}/Resources/opt/libtool/libexec/gnubin:$PATH"
@@ -90,17 +46,20 @@ if ! mkdir -p build ; then
   exit 1
 fi
 
-cd build
+cd build || exit 1
 
-if ! test -x ${SRCROOT}/configure ; then
+if ! test -x "${SRCROOT}/configure" ; then
   # Assume this to be a git checkout - generate required bootstrap files
-  if ! ${SRCROOT}/autogen.sh --no-configure-run ; then
+  if ! "${SRCROOT}/autogen.sh" --no-configure-run ; then
     echo "Autogen.sh failed" >&2
     exit 1
   fi
 fi
 
-if ! ${SRCROOT}/configure --prefix="${MAINDIR}/Contents" --bindir="${MAINDIR}/MacOS" \
+# TODO: Find out where the command really is, and get rid of this search
+export MOCCMD="$(find "${MAINDIR}/Resources/Cellar/qt" -name "moc" | head -n 1)"
+
+if ! "${SRCROOT}/configure" --prefix="${MAINDIR}/Contents" --bindir="${MAINDIR}/MacOS" \
  PATH="${MAINDIR}/Contents/Resources/bin:${MAINDIR}/Contents/Resources/opt/qt6/bin/:/usr/bin:/bin:/usr/sbin:/sbin:/Library/Apple/usr/bin" \
  LDFLAGS="-L${MAINDIR}/Contents/Resources/opt/qt6/lib -L${MAINDIR}/Contents/Resources/lib" \
  PKG_CONFIG_PATH="${MAINDIR}/Contents/Resources/opt/icu4c/lib/pkgconfig:$PKG_CONFIG_PATH" \
@@ -110,7 +69,7 @@ then
   exit 1
 fi
 
-if ! make -j$(sysctl -n hw.logicalcpu) ; then
+if ! make "-j$(sysctl -n hw.logicalcpu)" ; then
   echo "Freeciv make failed!" >&2
   exit 1
 fi
